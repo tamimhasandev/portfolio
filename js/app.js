6ec662698acc0deb51536ac414fd6e51f624ca08
tamimhasandev <ariyantamim@icloud.com>
2023-09-26 16:32:30 +0600

    Portfolio is ready  for publish

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// const bar = document.querySelector("menu > button");
+// const close = document.querySelector(".close > button");
+const body = document.querySelector("body");
+const menu = document.querySelector(".menu-list");
+const menuLInks = document.querySelectorAll(".menu-list > ul > li");
+
+function openMenu() {
+  menu.classList.add("show-menu");
+  body.style.overflow = "hidden";
+}
+function closeMenu() {
+  menu.classList.remove("show-menu");
+  body.style.overflow = "scroll";
+}
+
+for (let i = 0; i < menuLInks.length; i++) {
+  menuLInks[i].addEventListener("click", closeMenu);
+}
